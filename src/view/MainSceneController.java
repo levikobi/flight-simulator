f03bc332f91e3d0325c06885e40c33ca811d9658
kobi levi <kobilevi503@gmail.com>
2021-01-12 10:22:52 +0200

    Load autopilot script

--- src/view/MainSceneController.java
+++ src/view/MainSceneController.java
@@ -1,5 +1,21 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainSceneController {
+
+    public TextArea textarea;
+
+    public void loadAutopilotScript(ActionEvent actionEvent) throws IOException {
+        Path path = Paths.get("scripts/autopilot.txt");
+        String read = String.join("\n", Files.readAllLines(path));
+        textarea.setText(read);
+    }
 
 }
